d5e600e0b5c5498c1ebbd9de7b38b37efa298914
2x2Master#1240 <82650534+2x2master1@users.noreply.github.com>
2022-03-01 15:39:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ document.getElementById('snowflakeField').addEventListener('input', (event) => {
 
 function calcSnowfl() {
 	const value = parseInt(document.getElementById('snowflakeField').value);
-	if (value != '') {
+	if (value) {
 		console.log(value)
 		if (BigInt(value) >= (10n ** 16n) && BigInt(value) < (10n ** 19n)) {
 			document.querySelector('label').style.color = '#ffffff';
